fa52d0461383e70287c92b813ea68957ab5376f3
ngthanhtin <ngthanhtinqn@gmail.com>
2021-11-26 21:31:32 +0900

    implement convolve attention

--- attention/convolve_attention/attention.py
+++ attention/convolve_attention/attention.py
@@ -1,96 +1,38 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.nn.utils.weight_norm import weight_norm
 
 # Convolved Attention
 class ConvolvedAttention(nn.Module):
-    def __init__(self, img_feat_size, ques_feat_size, att_size, output_size, drop_ratio):
+    def __init__(self, num_sent_fc, img_feat_size, ques_feat_size, fc_size):
         super(ConvolvedAttention, self).__init__()
 
         self.img_feat_size = img_feat_size
         self.ques_feat_size = ques_feat_size
-        self.att_size = att_size
-        self.output_size = output_size
-        self.drop_ratio = drop_ratio
+        self.fc_size = fc_size
+
         self.layers = nn.ModuleList()
+        self.num_sent_fc = num_sent_fc
 
-        self.dropout = nn.Dropout(drop_ratio)
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Softmax(dim=1)
+        for i in range(num_sent_fc):
+            self.layers.append(nn.Linear(ques_feat_size, fc_size, bias=True))
 
-        self.fc11 = nn.Linear(ques_feat_size, att_size, bias=True)
-        self.fc12 = nn.Linear(img_feat_size, att_size, bias=False) #bias false
-        self.fc13 = nn.Linear(att_size, 1, bias=True)
+        self.attention_maps = None
 
-    def forward(self, img_feat, ques_feat, v_mask=True):
-        # import matplotlib.pyplot as plt
-        # import numpy as np
-        # import cv2
-        # test_att1 = img_feat.view((64, 8, 17)) # 64,8,17
-        # # test_att1 = test_att1.sum(0).unsqueeze(0)
-        # test_att1 = test_att1[0].unsqueeze(0)
-        # print(test_att1.shape)
-        # att_image = test_att1.permute(1, 2, 0).detach().numpy()
-        # att_image = cv2.resize(att_image, (300, 168)) 
-        # cv2.imshow("ads", att_image)
-        # cv2.waitKey(0)
+    def forward(self, img_feat, ques_feat):
+        """
+        img_feat: (B, C, W, H)
+        question_feat: (B, Dim) (last hidden layer of LSTM)
+        """
+        for i in range(self.num_sent_fc):
+            sent_fc = self.layers[i](ques_feat)
+            sent_fc = sent_fc.unsqueeze(2).unsqueeze(2) # B, 1, Dim, 1
+            attention = F.conv2d(img_feat, sent_fc, stride=(1,1))
+            
+            if i == 0:
+                self.attention_maps = attention
+            else:
+                self.attention_maps = torch.cat([self.attention_maps, attention], 0)
 
-        # exit()
-        # Batch size
-        B = ques_feat.size(0)
-
-        # Stack 1
-        ques_emb_1 = self.fc11(ques_feat)
-        img_emb_1 = self.fc12(img_feat)
-        # Compute attention distribution
-        h1 = self.tanh(ques_emb_1.view(B, 1, self.att_size) + img_emb_1)
-        h1_emb = self.fc13(self.dropout(h1))
-        # Mask actual bounding box sizes before calculating softmax
-        if v_mask:
-            mask = (0 == img_emb_1.abs().sum(2)).unsqueeze(2).expand(h1_emb.size())
-            h1_emb.data.masked_fill_(mask.data, -float('inf'))
-
-        p1 = self.softmax(h1_emb)
-        
-        #  Compute weighted sum
-        img_att_1 = img_emb_1*p1
-        # import matplotlib.pyplot as plt
-        # import numpy as np
-        # import cv2
-        
-        # test_att1 = img_att_1.view((1, 16, 16))
-        # # test_att1 = test_att1.sum(0).unsqueeze(0)
-        # att_image = test_att1.permute(1, 2, 0).detach().numpy()
-        # att_image = cv2.resize(att_image, (300, 168)) 
-        # cv2.imshow("ads", att_image)
-        # cv2.waitKey(0)
-
-        weight_sum_1 = torch.sum(img_att_1, dim=1)
-
-        # import matplotlib.pyplot as plt
-        # import numpy as np
-        # import cv2
-        
-        # test_att1 = weight_sum_1.view((1, 16, 16))
-        # # test_att1 = test_att1.sum(0).unsqueeze(0)
-        # att_image = test_att1.permute(1, 2, 0).detach().numpy()
-        # att_image = cv2.resize(att_image, (300, 168)) 
-        # cv2.imshow("ads", att_image)
-        # cv2.waitKey(0)
-
-        # Combine with question vector
-        u1 = ques_emb_1 + weight_sum_1
-
-        # Other stacks
-        us = []
-        ques_embs = []
-        img_embs  = []
-        hs = []
-        h_embs =[]
-        ps  = []
-        img_atts = []
-        weight_sums = []
-
-        us.append(u1)
-
-        return us[-1]
+        return self.attention_maps
