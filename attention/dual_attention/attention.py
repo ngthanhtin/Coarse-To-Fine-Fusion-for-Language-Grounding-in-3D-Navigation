648d56cc21fc05a4545c64950fd8d2a446b2c558
ngthanhtin <ngthanhtinqn@gmail.com>
2021-12-06 14:31:05 +0900

    add relu

--- attention/dual_attention/attention.py
+++ attention/dual_attention/attention.py
@@ -16,7 +16,9 @@ class DualAttention(nn.Module):
         self.attn_linear_1 = nn.Linear(self.vocab_size, 64) # 64 is the channel of visual features
         self.attn_linear_2 = nn.Linear(256, 64) # used for LSTM hidden features
 
-        self.softmax = nn.Softmax(dim = 1)
+        self.softmax = nn.Softmax(dim = -1)
+
+        self.attention_maps = [None, None]
 
     def make_bow_vector(self, sentence):
         vec = torch.zeros(self.vocab_size)
@@ -35,14 +37,17 @@ class DualAttention(nn.Module):
 
         return: attention features map: (B, C, W, H)
         """
+        B, C, H, W = img_feat.size(0), img_feat.size(1), img_feat.size(2), img_feat.size(3)
         # gated attention with BOW
         bow_vector = self.make_bow_vector(input_instr)
         bow_vector_linear = F.relu(self.attn_linear_1(bow_vector))
         bow_vector_linear = bow_vector_linear.unsqueeze(2).unsqueeze(3)
         bow_vector_linear = bow_vector_linear.expand(1, 64, 8, 17)
-        x_ga_1 = img_feat*bow_vector_linear
+        x_ga_1 = F.relu(img_feat*bow_vector_linear)
         x_ga_1 = torch.sum(x_ga_1, 1) # sum over channels
-        x_spat = self.softmax(x_ga_1)
+        
+        x_spat = self.softmax(x_ga_1.view(B, H*W)).view(B, 1, H, W)
+        self.attention_maps[0] = x_spat.data.clone()
 
         # gated attention with LSTM hidden features
         x_sa = x_spat * img_feat
@@ -50,6 +55,10 @@ class DualAttention(nn.Module):
         text_feat_linear = text_feat_linear.unsqueeze(2).unsqueeze(3)
         text_feat_linear = text_feat_linear.expand(1, 64, 8, 17)
         x_ga_2 = x_sa*text_feat_linear
+
+        # x_spat_2 = x_ga_2.sum(3).sum(2)# sum over width and height
+        # x_spat_2 = self.softmax(x_spat_2.view(B, H*W)).view(B, 1, H, W)
+        # self.attention_maps[1] = x_spat_2.data.clone()
 
         return x_ga_2
 
